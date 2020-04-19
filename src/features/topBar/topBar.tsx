c34bab6ba9bc59b3b4916e9d30860eee27b80843
Matthew Smith <mattdsmith228@gmail.com>
2020-04-19 01:05:47 -0400

    twitch login added

--- src/features/topBar/topBar.tsx
+++ src/features/topBar/topBar.tsx
@@ -2,7 +2,10 @@ import React from 'react';
 import styles from './topBar.module.css';
 
 export function TopBar() {
-
+    function onClick(){
+        window.location.href="https://id.twitch.tv/oauth2/authorize?response_type=code&client_id=mb8hd3bpd9kuybk2gpvlcz6jw4ebyn&redirect_uri=https%3A%2F%2Fmanage.mclarkdev.com%2Fpwr%2Fauth/&scope=user_read";
+        
+    }
     return (
 
         <div className={styles.container}>
@@ -19,7 +22,7 @@ export function TopBar() {
             </div>
 
             <div className={styles.loginContainer}>
-                <button className={styles.loginBtn} type="button">Login to Twitch</button>
+                <button className={styles.loginBtn} type="button" onClick={onClick}>Login to Twitch</button>
             </div>
 
         </div>
